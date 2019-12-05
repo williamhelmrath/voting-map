3857bb11b7cc0a993ecb22c51caf3289fb8b0b22
jguevarra <jfg3xs@virginia.edu>
2019-12-05 17:21:07 -0500

    edited UI on home page with a more relevant picture LOL

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,11 +18,11 @@ const Home = () => {
         <div>
 
             {/* add parallax or some cool image stuffs :0 */}
-            <Container maxWidth="sm" component="main" className={classes.heroContent}>
+            <Container component="main" className={classes.heroContent}>
                 <Parallax
                     // blur={10}
-                    bgImage={require('./Images/baby-yoda.jpg')}
-                    bgImageAlt="baby yoda"
+                    bgImage={require('./Images/voting4.jpg')}
+                    bgImageAlt="voting"
                     strength={200}
                 >
                     <Typography
@@ -32,32 +32,34 @@ const Home = () => {
                         color="inherit"
                         style={{ 
                             color: "black", 
-                            align: "center"}}
+                            align: "center",
+                            // fontFamily: "Ariel"
+                        }}
                         // gutterBottom
                     >
                         Welcome
                     </Typography>
+                    <div style={{ height: '600px' }} />
+                </Parallax>
+            </Container>
 
-                    <Typography
+            <Typography
                         variant="h5"
                         align="center"
-                        // color="textSecondary"
+                        color="textSecondary"
                         component="p"
-                        style={{ 
-                            position: "absolute", 
-                            bottom: "100px", 
-                            color: "black", 
-                            backgroundColor: "gray", 
-                            opacity: 0.6
-                        }}
+                        // style={{ 
+                        //     position: "absolute", 
+                        //     bottom: "100px", 
+                        //     color: "black", 
+                        //     backgroundColor: "gray", 
+                        //     opacity: 0.6
+                        // }}
                     >
                         Try the interactive map that allows you to check voting location information
                         across the United States!
                         {/* <div style={{ position: "absolute", bottom: "10px"}}/> */}
                     </Typography>
-                    <div style={{ height: '500px' }} />
-                </Parallax>
-            </Container>
 
 
         </div>
