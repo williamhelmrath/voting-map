51789bd14dec82e21767641f77c52ff8fee3a478
jguevarra <jfg3xs@virginia.edu>
2019-12-06 00:23:26 -0500

    edited map page

--- src/Components/pollLocation.js
+++ src/Components/pollLocation.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-
-// UI/CSS imports
 import MapComp from "./MapComp.js";
 import SearchBar from "./SearchBar.js";
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
 
 export default class pollLocation extends Component {
 
@@ -16,20 +17,40 @@ export default class pollLocation extends Component {
 
   render() {
     return (
-      <div>
+        <div>
 
-        <SearchBar parentCallback={this.callbackFunction} />
+            <Container maxWidth="lg" component="main" style={{padding:"65px"}}>
+                <Typography
+                    component="h1"
+                    variant="h2"
+                    align="center"
+                    color="textPrimary"
+                    gutterBottom
+                >
+                    Find Your Poll Location
+                </Typography>
+                <Typography
+                    variant="h5"
+                    align="center"
+                    color="textSecondary"
+                    component="p"
+                >
+                    Drag the marker on the map to any location OR type your address in the search bar and click the icon
+                </Typography>
+            </Container>
 
-        <div
-          style={{
-            alignItems: "stretch",
-            justifyContent: "center",
-            alignSelf: "stretch"
-          }}
-        >
-          <MapComp searchBarTerm={this.state.textFieldAddress} />
+            <SearchBar parentCallback={this.callbackFunction} /> <br></br><br></br>
+
+            <div
+                style={{
+                    alignItems: "stretch",
+                    justifyContent: "center",
+                    alignSelf: "stretch"
+                }}
+            >
+                <MapComp searchBarTerm={this.state.textFieldAddress} />
+            </div>
         </div>
-      </div>
     );
   }
 }
